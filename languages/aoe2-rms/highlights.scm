c17626430551887645d33447e1ad1b77bdbf629e
twestura <twestura@gmail.com>
2026-03-03 06:35:57 -0500

    Initial define and constant highlights.

--- languages/aoe2-rms/highlights.scm
+++ languages/aoe2-rms/highlights.scm
@@ -1,2 +1,14 @@
 ; Highlights for Age of Empires II random map scripts.
-; TODO: add highlight rules
+
+; Keywords
+"#define" @keyword
+"#const" @keyword
+
+; Identifiers after #define become label names
+(define_statement
+  (identifier) @constant)
+
+; Identifiers after #const become constant names, integer is the value
+(const_statement
+  (identifier) @constant
+  (integer) @number)
