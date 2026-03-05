011ae578804901ec8c6dd494e66bbbfea07c5d3f
twestura <twestura@gmail.com>
2026-03-05 02:12:56 -0500

    Add brackets and indents files.

---
+++ languages/aoe2-rms/indents.scm
@@ -0,0 +1,5 @@
+(land_generation_command "}" @end) @indent
+(elevation_generation_command "}" @end) @indent
+(terrain_generation_command "}" @end) @indent
+(connection_generation_command "}" @end) @indent
+(objects_generation_command "}" @end) @indent
