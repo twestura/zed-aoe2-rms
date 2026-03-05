fa25fd54c8cda5c8a370f61e758b48865d8a2096
twestura <twestura@gmail.com>
2026-03-05 02:15:08 -0500

    Remove brackets and indents files.

--- languages/aoe2-rms/indents.scm
+++
@@ -1,5 +0,0 @@
-(land_generation_command "}" @end) @indent
-(elevation_generation_command "}" @end) @indent
-(terrain_generation_command "}" @end) @indent
-(connection_generation_command "}" @end) @indent
-(objects_generation_command "}" @end) @indent
